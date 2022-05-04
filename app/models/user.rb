ecbf3afbada0132e50ebfa482b77af8c3854bc1a
EC2 Default User <ec2-user@ip-172-31-22-121.ec2.internal>
2022-05-04 09:11:54 +0000

    [Add]

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,32 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
+  # 自分がフォローされる（被フォロー）側の関係性
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # 被フォロー関係を通じて参照→自分をフォローしている人
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  # 自分がフォローする（与フォロー）側の関係性
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # 与フォロー関係を通じて参照→自分がフォローしている人
+  has_many :followings, through: :relationships, source: :followed
+  
+  
   validates :name, presence: true, uniqueness: true, length: { minimum: 2, maximum: 20 }
   validates :introduction, length: { maximum: 50 }
 
 
+  # ユーザーをフォローする
+  def follow(user)
+    relationships.create(followed_id: user.id)
+  end
+    # ユーザーのフォローを外す
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
+  end
+    # フォロー確認をおこなう
+  def following?(user)
+    followings.include?(user)
+  end
 
   def get_profile_image
    (profile_image.attached?) ? profile_image : 'no_image.jpg'
